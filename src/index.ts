c5a5b5ad4f40ed3d7510b9a59af887376129cb00
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-15 16:08:51 +0200

    using module os demo

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
 import "dotenv/config"
+import os from "node:os"
 import logger from "./logger.js"
-function getParameters(): number[] {
-    const firstNumberStr = process.argv[2];
-    const firstNumber = processArgument(firstNumberStr, "first")
-    logger.debug(`firstNumber is ${firstNumber}`)
-    const secondNumberStr = process.argv[3];
-    const secondNumber = processArgument(secondNumberStr, "second")
-    logger.debug(`secondNumber is ${secondNumber}`)
-   
-    return [firstNumber, secondNumber]
-} 
-function sum(firstNumber: number, secondNumber: number): number {
-    logger.debug(`function sum arguments : ${firstNumber} and ${secondNumber}`)
-    return firstNumber + secondNumber
-}
-function main() {
-   try {
-     const numbers: number[] = getParameters()
-     logger.debug(`parameters of main are ${numbers}`)
-     const result: number = sum(numbers[0] || 0, numbers[1] || 0)
-     console.log(`sum of ${numbers[0]} and ${numbers[1]} is ${result}`)
- }
-    catch (error) {
-        const errorMessage = error as Error
-        logger.error(errorMessage.message)
-   }
-}
-function processArgument(param: string | undefined, seqNum: string): number {
-    if(!param) {
-        throw Error(seqNum +  " number must be provided in arguments")
-    }
-    const num = Number(param);
-    
-    if (isNaN(num)) {
-        throw Error(seqNum + " argument must be number")
-    }
-    return num
-}
-main();
+logger.info(`platform: ${os.platform()}, free memory: ${os.freemem()/(1024*1024*1024)} Gb\
+  total memory: ${os.totalmem()/(1024*1024*1024)} Gb`)
