8eb8f13859a580602c473348005968de32ef7933
Yaroslav Benin <dondublon1@yandex.ru>
2026-03-22 12:39:23 +0200

    node server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import "dotenv/config"
 import os from "node:os"
 import logger from "./logger.js"
-logger.info(`platform: ${os.platform()}, free memory: ${os.freemem()/(1024*1024*1024)} Gb\
-  total memory: ${os.totalmem()/(1024*1024*1024)} Gb`)
+import * as http from "node:http"
+//import { Server } from "socket.io"
+//import { WebSocketServer } from "ws"
+import {createServer} from "node:net";
+
+const server = createServer()
+const port = Number(process.env.PORT) || 3000
+server.listen(port, () => {
+    logger.info(`Server is listening on port ${port}`)
+    logger.info(`Hostname: ${os.hostname()}`)
+})
+server.on("request", (req, res) => {
+    res.statusCode = 200
+    res.write("ok")
+})
